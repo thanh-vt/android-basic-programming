c4be4fc6b80b67d2c0270ec8218ab0b61d3cc09a
thanhvt <thanhvt@vissoft.vn>
2023-01-01 00:01:28 +0700

    resolve merge conflicts

--- app/src/main/java/com/thanhvt/todo/activity/MyServiceFragment.kt
+++ app/src/main/java/com/thanhvt/todo/activity/MyServiceFragment.kt
@@ -1,21 +1,16 @@
 package com.thanhvt.todo.activity
 
 import android.annotation.SuppressLint
-import android.app.*
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.NotificationCompat
+import androidx.fragment.app.Fragment
 import com.thanhvt.todo.BuildConfig
-import com.thanhvt.todo.R
 import com.thanhvt.todo.databinding.FragmentMyServiceBinding
 
 // TODO: Rename parameter arguments, choose names that match
